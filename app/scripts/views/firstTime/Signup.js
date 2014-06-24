09abb9cba1c912c2fe5a435883fde6fd1f85ed14
Simon Soriano <simon0191@gmail.com>
2014-06-24 00:00:27 -0500

    Add signup form skeleton

--- app/scripts/views/firstTime/Signup.js
+++ app/scripts/views/firstTime/Signup.js
@@ -1,16 +1,17 @@
 define(function(require) {
-	'use strict';
-	var $ = require('jquery'),
-        Handlebars = require('handlebars'),
-        Backbone = require('backbone'),
-        templates = require('templates'),
+  'use strict';
+  var $ = require('jquery'),
+      Handlebars = require('handlebars'),
+      Backbone = require('backbone'),
+      templates = require('templates'),
 
-        firstTimeHome = templates['firstTime.signup'];
+      firstTimeHome = templates['firstTime.signup'];
 
    return Backbone.View.extend({
       el: '#content',
       render: function() {            
           this.$el.html(firstTimeHome());
       }
+      //TODO: add validations to form
     });
 });
